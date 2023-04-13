096b3736f6701f20ab700433233a4b376650a843
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-13 23:58:27 +0200

    returned fragment from createHtml function

--- scripts.js
+++ scripts.js
@@ -46,16 +46,16 @@ const MONTHS = [
               time: [10, 8, 3, 12],
             },
             {
-              date: '2022-11-25T22:00:00.000Z',
-              time: [6, 8, 9, 11],
-            },
-            {
               date: '2022-12-02T22:00:00.000Z',
               time: [10, 11, 4, 8],
             },
             {
               date: '2022-12-09T22:00:00.000Z',
               time: [9, 8, 9, 11],
+            },
+            {
+              date: '2022-11-25T22:00:00.000Z',
+              time: [6, 8, 9, 11],
             },
           ],
         },
@@ -64,46 +64,54 @@ const MONTHS = [
   };
   
   // Only edit below this comment
-  
+
   const createHtml = (athlete) => {
-    firstName, surname, id, races = athlete
-    [date], [time] = races.reverse()
+    const firstName = athlete.firstName
+    const surname = athlete.surname
+    const id = athlete.id
+    const races = athlete.races
+    const [date, {time}] = races.reverse()
   
     const fragment = document.createDocumentFragment();
   
-    title = document.createElement(h2);
-    title= id;
+    const title = document.createElement("h2");
+    title.innerHTML = id;
     fragment.appendChild(title);
   
-    const list = document.createElement(dl);
+    const list = document.createElement("dl");
   
-    const day = date.getDate();
-    const month = MONTHS[date.month];
-    const year = date.year;
+    const day = new Date(date.date).getDate();
+    const month = MONTHS[new Date(date.date).getMonth()];
+    const year = new Date(date.date).getFullYear();
   
-    first, second, third, fourth = timeAsArray;
-    total = first + second + third + fourth;
+    const [first, second, third, fourth] = time//timeAsArray;
+    const total = first + second + third + fourth;
   
     const hours = total / 60;
     const minutes = total / hours / 60;
-  
+
     list.innerHTML = /* html */ `
       <dt>Athlete</dt>
-      <dd>${firstName surname}</dd>
+      <dd>${firstName} ${surname}</dd>
   
       <dt>Total Races</dt>
-      <dd>${races}</dd>
+      <dd>${races.length}</dd>
   
       <dt>Event Date (Latest)</dt>
-      <dd>${day month year}</dd>
+      <dd>${day} ${month} ${year}</dd>
   
       <dt>Total Time (Latest)</dt>
-      <dd>${hours.padStart(2, 0) minutes}</dd>
+      <dd>${Math.round(hours).toString().padStart(2, "0")}:${Math.round(minutes).toString().padStart(2, "0")}</dd>
     `;
   
     fragment.appendChild(list);
+
+    return fragment
   }
   
-  [NM372], [SV782] = data
-  document.querySelector(NM372).appendChild(createHtml(NM372));
-  document.querySelector(SV782).appendChild(createHtml(SV782));
+  //[NM372], [SV782] = data
+  const NM372 = data.response.data.NM372
+  const SV782 = data.response.data.SV782
+
+  document.querySelector('[data-athlete="NM372"]').appendChild(createHtml(NM372));
+  document.querySelector('[data-athlete="SV782"]').appendChild(createHtml(SV782));
